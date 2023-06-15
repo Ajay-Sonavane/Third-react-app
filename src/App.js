5ad6d82961526831877a7fac0ba002e818924be2
Ajay Sonavane <ajaysonawane267@gmail.com>
2023-06-15 20:13:58 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function App() {
   return (
     <>
@@ -9,11 +11,12 @@ function App() {
 export default App;
 
 function CounterApp() {
-  let counter = 1;
-
+  //let counter = 1; stateless data member == means if we change the numer it will not afffecd in Gui
+  let [counter, setCounter] = useState(1); //stateful// this will help to perform dom operations.
   let LikemeAction = () => {
     counter = counter + 1;
     console.log("i m button ", counter);
+    setCounter(counter); //calling setcounter is to perform Dom operation
   };
 
   return (
