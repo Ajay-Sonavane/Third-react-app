f82834adeec6db9d761ac997e6a439abef8aceb8
Ajay Sonavane <ajaysonawane267@gmail.com>
2023-06-15 22:42:41 +0530

    basic counter

--- src/App.js
+++ src/App.js
@@ -8,21 +8,28 @@ function App() {
     </>
   );
 }
-export default App;
 
 function CounterApp() {
-  //let counter = 1; stateless data member == means if we change the numer it will not afffecd in Gui
-  let [counter, setCounter] = useState(1); //stateful// this will help to perform dom operations.
-  let LikemeAction = () => {
-    counter = counter + 1;
-    console.log("i m button ", counter);
-    setCounter(counter); //calling setcounter is to perform Dom operation
-  };
+  //let counter = 1 ;stateless memeber
+  // let list = useState(1);
+  // let[counter,setCounter]= list       // first is variable and second is method
+  //let [counter, setCounter] = useState(1);
 
+  //let counter = 1; stateless
+  let [counter, setCounter] = useState(100); //if you want to perform dom operation then use of function is mandatory
+
+  let likemAction = () => {
+    counter = counter + 1;
+    console.log(counter);
+    setCounter(counter);
+  };
   return (
     <>
       <h1>{counter}</h1>
-      <input type="button" value="Likeme" onClick={LikemeAction} />
+      <h1>{counter}</h1>
+      <h1>{counter}</h1>
+      <input type="button" value="Like Me" onClick={likemAction} />
     </>
   );
 }
+export default App;
