7c951df89bc363fca35ea002979834a3666f235c
Ajay Sonavane <ajaysonawane267@gmail.com>
2023-06-15 17:10:51 +0530

    template

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 function App() {
   return (
     <>
-      <h1 className="bg-success-subtle text-black">Hello World</h1>
+      <AppHeader />
+      <Body />
+      <AppFooter />
+    </>
+  );
+}
+//user defined tag :-
+function AppHeader() {
+  return (
+    <>
+      <h1>AppHeader</h1>
+    </>
+  );
+}
+
+function Body() {
+  return (
+    <>
+      <h1>Body</h1>
+    </>
+  );
+}
+function AppFooter() {
+  return (
+    <>
+      <h1>AppFooter</h1>
     </>
   );
 }
