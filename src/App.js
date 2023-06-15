bae10a7974136020c4dbebb7ecac98f9df543535
Ajay Sonavane <ajaysonawane267@gmail.com>
2023-06-15 17:29:59 +0530

    body

--- src/App.js
+++ src/App.js
@@ -23,7 +23,20 @@ function AppHeader() {
 function Body() {
   return (
     <>
-      <h1>Body</h1>
+      <div className="alert alert-success fs-3">
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+        voluptatum, enim reiciendis amet ipsam iusto ea neque. Nobis molestiae
+        non velit magni debitis, culpa sequi corporis ipsa cupiditate
+        perferendis dolorum eius, doloremque, laboriosam labore! Voluptate
+        veniam esse earum libero rerum, animi natus illo. Tempore facere beatae,
+        saepe repellat molestiae non delectus? Quas autem tempore saepe itaque.
+        Repellendus ab illo dolore, sunt amet saepe blanditiis laudantium. Nihil
+        soluta nemo nesciunt delectus animi odit in neque officia? Dolor,
+        beatae? Esse accusamus dolorem explicabo quae, facere veritatis vel
+        doloribus voluptas, amet voluptate officiis nulla dolorum reiciendis ab
+        quod ullam sint deserunt consectetur blanditiis!
+      </div>{" "}
     </>
   );
 }
